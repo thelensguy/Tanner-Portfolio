281a812ca378ccbf48355337672487c0b1a9acc6
Than Nguyenvu <thelensguy@gmail.com>
2020-07-20 13:51:07 -0700

    Formatted intro

--- tanner-portfolio/src/app/landing/landing.component.ts
+++ tanner-portfolio/src/app/landing/landing.component.ts
@@ -8,11 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    const logo = document.querySelectorAll('path');
-    console.log(logo);
-    for (let i = 0; i < logo.length; i++) {
-      console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-    }
-  }
+  ngOnInit() {}
 }
